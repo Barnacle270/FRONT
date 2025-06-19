01d8c4331c780f6dcc0f6574a8d1ad769650a111
Jhostym <jhosrohe3@gmail.com>
2025-06-19 10:47:35 -0500

    suiendo rep

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage"
 import HomePage from "./pages/HomePage"
 import ProfileTask from "./pages/ProfilePage"
@@ -9,29 +9,29 @@ import Navbar from "./components/Navbar"
 import { BoletaProvider } from "./context/BoletasContext"
 import BoletasPage from "./pages/BoletasPage"
 import RegisterPage from "./pages/RegisterPage"
-
 function App() {
   return (
     <AuthProvider>
       <BoletaProvider>
-          <Router>
-            <main className="container mx-auto px-2">
-              <Navbar />
-              <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
+        <Router>
+          <main className="container mx-auto px-2">
+            <Navbar />
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
 
-                <Route element={<ProtectedRoute />}>
-                  <Route path="/" element={<HomePage />} />
-                  <Route path="/profile" element={<ProfileTask />} />
 
-                  <Route path="/add-boletas" element={<BoletasFormPage />} />
-                  <Route path="/boletas" element={<BoletasPage />} />
-  
-                </Route>
-              </Routes>
-            </main>
-          </Router>
+              <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/profile" element={<ProfileTask />} />
+
+                <Route path="/add-boletas" element={<BoletasFormPage />} />
+                <Route path="/boletas" element={<BoletasPage />} />
+
+              </Route>
+            </Routes>
+          </main>
+        </Router>
       </BoletaProvider>
     </AuthProvider>
   );
